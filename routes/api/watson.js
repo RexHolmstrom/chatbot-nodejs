c6e7b7e92c15737ce28ba96c68b229953d18661e
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-07 13:46:37 +0200

    api update

--- routes/api/watson.js
+++ routes/api/watson.js
@@ -2,3 +2,15 @@ const express = require("express");
 const router = express.Router();
 const AssistantV2 = require("ibm-watson/assistant/v2");
 const { IamAuthenticator } = require("ibm-watson/auth");
+
+//
+//
+const Authenticator = new IamAuthenticator({
+  apikey: process.env.WATSON_ASSISTANT_APIKEY,
+});
+
+const assistant = new AssistantV2({
+  version: "2019-02-28",
+  authenticator: authenticator,
+  url: WATSON_ASSISTANT_URL,
+});
