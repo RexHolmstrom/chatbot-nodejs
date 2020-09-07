bc1f24f36bac5f23f9882aa268bd5703e3f95eca
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-07 12:58:50 +0200

    watson setup

--- routes/api/watson.js
+++ routes/api/watson.js
@@ -1,1 +1,4 @@
 const express = require("express");
+const router = express.Router();
+const AssistantV2 = require("ibm-watson/assistant/v2");
+const { IamAuthenticator } = require("ibm-watson/auth");
