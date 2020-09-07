f31667d5e875372701f8858370f6197d58a8e41f
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-07 12:55:35 +0200

    watson setup

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("App is listening to ", port);
-  console.log("Hello Server");
+  console.log("Hello Server, how are you?");
 });
