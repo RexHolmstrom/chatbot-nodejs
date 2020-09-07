cf82196f4844bc468aeab5627faa40ccfafeb6b0
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-07 13:52:21 +0200

    route handle, api update

--- routes/api/watson.js
+++ routes/api/watson.js
@@ -4,7 +4,7 @@ const AssistantV2 = require("ibm-watson/assistant/v2");
 const { IamAuthenticator } = require("ibm-watson/auth");
 
 //
-//
+// Connecting to assistant
 const Authenticator = new IamAuthenticator({
   apikey: process.env.WATSON_ASSISTANT_APIKEY,
 });
@@ -13,4 +13,18 @@ const assistant = new AssistantV2({
   version: "2019-02-28",
   authenticator: authenticator,
   url: WATSON_ASSISTANT_URL,
+});
+
+//
+// Route to handle session
+router.get("/session", async (req, res) => {
+  try {
+    const session = await assistant.createSession({
+      assistantId: process.env.WATSON_ASSISTANT_ID,
+    });
+    res.json(session["result"]);
+  } catch (err) {
+    res.send("There was an error!");
+    console.log(err);
+  }
 });
